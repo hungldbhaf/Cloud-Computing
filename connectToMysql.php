fab657a778c6c89281eb3394242f8cace0dc03aa
hungldbhaf <102125714+hungldbhaf@users.noreply.github.com>
2022-04-18 20:52:50 +0700

    Add files

---
+++ connectToMysql.php
@@ -0,0 +1,10 @@
+<?php
+$GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "gadgetstore");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$connecti=mysqli_connect("localhost", "root", "", "gadgetstore");
+$link = mysqli_connect("localhost", "root", "", "gadgetstore");
+?>
+
